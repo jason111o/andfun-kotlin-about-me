a58aa9f477cefb7a8df78ecbc1d6c1daad212e96
jason111o <razrjlinux@gmail.com>
2021-03-07 05:54:20 -0600

    TODO's DONE

--- app/src/main/java/com/example/android/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/android/aboutme/MainActivity.kt
@@ -16,7 +16,15 @@
 
 package com.example.android.aboutme
 
+import android.app.Activity
+import android.content.Context
+import android.media.session.MediaSession
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -32,10 +40,29 @@ import androidx.appcompat.app.AppCompatActivity
  */
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var doneButton: Button
+    private lateinit var editText: EditText
+    private lateinit var nicknameTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // TODO (04) Add a click handler for the Done button to process the input and update the visibility of the views
+//        initialize viewa
+        doneButton = findViewById(R.id.done_button)
+        editText = findViewById(R.id.edit_text)
+        nicknameTextView = findViewById(R.id.nickname_text_view)
+
+        // TODO DONE (04) Add a click handler for the Done button to process the input and update the visibility of the views
+        doneButton.setOnClickListener {
+            nicknameTextView.text = editText.editableText
+            editText.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameTextView.visibility = View.VISIBLE
+
+//            close the keyboard on button click
+            val keyboard = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            keyboard.hideSoftInputFromWindow(it.windowToken,0)
+        }
     }
 }
